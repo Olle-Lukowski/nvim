5c062451eff7bbf72f1cd499775d292e69ffc9f7
Olle-Lukowski <ollelukowski@hotmail.com>
2023-12-12 18:18:22 +0100

    Setup

---
+++ lua/olle/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  config = (function()
+    vim.cmd.colorscheme('catppuccin')
+
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  end)
+}
