c10ba64e3cf305536aec3cd92562648331cf1515
Olle Lukowski <ollelukowski@hotmail.com>
2024-12-05 14:33:45 +0100

    feat(icons): remove `nvim-web-devicons`
    We use the `mini.icons` mocking functionality to replace it.

--- lua/plugins/mini-icons.lua
+++ lua/plugins/mini-icons.lua
@@ -1,5 +1,16 @@
 return {
-  'echasnovski/mini.icons',
-  lazy = true,
-  config = true,
+  {
+    "echasnovski/mini.icons",
+    opts = {},
+    lazy = true,
+    specs = {
+      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
 }
