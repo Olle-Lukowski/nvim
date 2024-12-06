8733b16a751ab1212f7bd43fbaeb4710914d1889
Olle Lukowski <ollelukowski@hotmail.com>
2024-12-06 22:38:48 +0100

    feat: add `mini.files`

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'echasnovski/mini.files',
+    keys = {
+      {
+        '-',
+        function() require('mini.files').open() end,
+        desc = 'Open file system (mini.files)',
+      },
+    },
+  },
+}
